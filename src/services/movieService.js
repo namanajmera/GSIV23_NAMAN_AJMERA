657d9256458186de0621809d2ed6800a5cb01f79
Naman Ajmera <naman.ajmera@magicsw.com>
2023-08-19 17:23:40 +0530

    Added: Show the upcoming movies.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,7 +7,7 @@ const headers = {
     "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmJmNGRhYjRiMTE5MjUyYTRlZjlmNWIwNjkwZjEyOSIsInN1YiI6IjY0ZGYzYzEyZDEwMGI2MTRiMzAwY2IxNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Fc3Q71tOlZ_95aIrE0KoPWWj4EoR_EF_LzI6sKiIl3o",
 };
 const getMoviesDetails = (page) => {
-  const apiURL = `${BASE_URL}movie/now_playing?language=en-US&page=${page}`;
+  const apiURL = `${BASE_URL}movie/upcoming?language=en-US&page=${page}`;
 
   return axios.get(apiURL, { headers });
 };
