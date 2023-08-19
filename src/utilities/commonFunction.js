3bb5bed7519e02ddb0ff2712a2df68ad660b67db
Naman Ajmera <naman.ajmera@magicsw.com>
2023-08-19 17:20:26 +0530

    Added: Some more common FUnction.

--- src/utilities/commonFunction.js
+++ src/utilities/commonFunction.js
@@ -5,5 +5,23 @@ export const convertMinutesToHHMM = (minutes) => {
   const remainingMinutes = minutes % 60;
   const formattedHours = hours.toString().padStart(2, "0");
   const formattedMinutes = remainingMinutes.toString().padStart(2, "0");
-  return `${formattedHours}h:${formattedMinutes}m`;
+  return `${formattedHours}h : ${formattedMinutes}m`;
+};
+
+export const formatToTwoDecimalPlaces = (number) => {
+  if (typeof number !== "number") {
+    throw new Error("Input must be a number");
+  }
+  return parseFloat(number.toFixed(1));
+};
+
+export const extractYearFromDate = (dateString) => {
+  // Split the date string by '-' to get an array of parts
+  const dateParts = dateString.split("-");
+
+  // Extract the year part (assuming the year is the first part)
+  const year = parseInt(dateParts[0], 10);
+
+  // Return the year as a number
+  return year;
 };
